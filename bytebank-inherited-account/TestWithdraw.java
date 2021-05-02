c8bcf964ab6b8805a08ef74a9c6d23e65cb3b261
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-02 16:59:03 -0300

    chore: adding error treatment at unchecked exception

--- bytebank-inherited-account/TestWithdraw.java
+++ bytebank-inherited-account/TestWithdraw.java
@@ -5,18 +5,34 @@ public class TestWithdraw {
 		Account c1 = new CheckingAccount(123, 456);
 		
 		c1.deposit(200.0);
-		c1.withdraw(290.0);
+		
+		try {
+			c1.withdraw(290.0);
+		} catch(InsufficientBalanceException ex) {
+			System.out.println("Exception: " + ex.getMessage());
+		}
+		
 		
 		System.out.println(c1.getBalance());
 	}
 }
 
+
 //console:
+// without error treatment:
 
 // O total de contas é 1
 // Exception in thread "main" InsufficientBalanceException: Saldo: 200.0, Valor a sacar: 290.2
 // 	at Account.withdraw(Account.java:23)
 // 	at CheckingAccount.withdraw(CheckingAccount.java:11)
 // 	at TestWithdraw.main(TestWithdraw.java:9)
+
+
+//with error treatment:
+
+// O total de contas é 1
+// Exception: Saldo: 200.0, Valor a sacar: 290.2
+// 200.0
+
 
 //unchecked!
